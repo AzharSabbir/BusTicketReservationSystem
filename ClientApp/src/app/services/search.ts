5107a869f083a3f42903eae5849f9483f09c05a1
AzharSabbir <azharsabbir25@gmail.com>
2025-10-23 22:39:24 +0600

    SearchForm has been added to the frontend, and the frontend successfully calls the backend.

---
+++ ClientApp/src/app/services/search.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface AvailableBusDto {
+  busScheduleId: string;
+  companyName: string;
+  busName: string;
+  startTime: Date;
+  arrivalTime: Date;
+  seatsLeft: number;
+  price: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  private apiUrl = '/api/search';
+
+  constructor(private http: HttpClient) { }
+
+  searchAvailableBuses(from: string, to: string, journeyDate: string): Observable<AvailableBusDto[]> {
+
+    let params = new HttpParams()
+      .set('from', from)
+      .set('to', to)
+      .set('journeyDate', journeyDate);
+
+    return this.http.get<AvailableBusDto[]>(`${this.apiUrl}/available-buses`, { params });
+  }
+}
